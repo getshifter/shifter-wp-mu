28ad47b564c9431bfd463c2e233f0b29a33c4b91
Daniel Olson <emaildano@gmail.com>
2019-04-26 12:17:53 -0400

    added wp-coding-standards, fixed errors

--- api/class-shifter-admin.php
+++ api/class-shifter-admin.php
@@ -44,79 +44,83 @@ class Shifter_API {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param      string $plugin_name       The name of this plugin.
+	 * @param      string $version    The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+		$this->version     = $version;
 
 		// API vars
-		$this->site_id = getenv("SITE_ID");
-		$this->access_token = getenv("SHIFTER_ACCESS_TOKEN");
-		$this->refresh_token = getenv("SHIFTER_REFRESH_TOKEN");
+		$this->site_id       = getenv( 'SITE_ID' );
+		$this->access_token  = getenv( 'SHIFTER_ACCESS_TOKEN' );
+		$this->refresh_token = getenv( 'SHIFTER_REFRESH_TOKEN' );
 
-		$shifte_api_v1 = getenv("SHIFTER_API_URL_V1");
-		$shifte_api_v2 = getenv("SHIFTER_API_URL_V2");
-		$this->terminate_url = "$shifte_api_v2/projects/$this->site_id/wordpress_site/stop";
-		$this->generate_url = "$shifte_api_v1/projects/$this->site_id/artifacts";
-		$this->refresh_url = "$shifte_api_v1/login";
+		$shifte_api_v1               = getenv( 'SHIFTER_API_URL_V1' );
+		$shifte_api_v2               = getenv( 'SHIFTER_API_URL_V2' );
+		$this->terminate_url         = "$shifte_api_v2/projects/$this->site_id/wordpress_site/stop";
+		$this->generate_url          = "$shifte_api_v1/projects/$this->site_id/artifacts";
+		$this->refresh_url           = "$shifte_api_v1/login";
 		$this->shifter_dashboard_url = "https://go.getshifter.io/admin/sites/$this->site_id";
 
-		$bootup_unixtimestamp = file_get_contents(ABSPATH."/.bootup");
-		$bootup_date = new DateTime();
-		self::$token_update_date = $bootup_date->setTimestamp(intval($bootup_unixtimestamp));
+		$bootup_unixtimestamp    = file_get_contents( ABSPATH . '/.bootup' );
+		$bootup_date             = new DateTime();
+		self::$token_update_date = $bootup_date->setTimestamp( intval( $bootup_unixtimestamp ) );
 
 	}
 
-	private $site_id = '';
-	private $generate_url = '';
+	private $site_id       = '';
+	private $generate_url  = '';
 	private $terminate_url = '';
-	private $access_token = '';
+	private $access_token  = '';
 	private $refresh_token = '';
 	private static $token_update_date;
 
 	public function terminate_wp_app() {
-		if ($this->access_token_is_expired()) {
+		if ( $this->access_token_is_expired() ) {
 			$this->refresh_token();
 		}
-		wp_remote_request($this->terminate_url, $this->build_args());
+		wp_remote_request( $this->terminate_url, $this->build_args() );
 	}
 
 	public function generate_wp_app() {
-		if ($this->access_token_is_expired()) {
+		if ( $this->access_token_is_expired() ) {
 			$this->refresh_token();
 		}
-		return wp_remote_request($this->generate_url, $this->build_args());
+		return wp_remote_request( $this->generate_url, $this->build_args() );
 	}
 
 	private function build_args() {
 		$headers = array(
-			"authorization" => $this->access_token,
-			"content-Type" => "application/json"
+			'authorization' => $this->access_token,
+			'content-Type'  => 'application/json',
 		);
-		return array("method" => "POST", "headers" => $headers, "blocking" => false);
+		return array(
+			'method'   => 'POST',
+			'headers'  => $headers,
+			'blocking' => false,
+		);
 	}
 
 	private function refresh_token() {
-		$headers = array("content-type" => "application/json");
-		$args = array(
-			"method" => "PUT",
-			"headers" => $headers,
-			"body" => json_encode(array("refreshToken" => $this->refresh_token))
+		$headers            = array( 'content-type' => 'application/json' );
+		$args               = array(
+			'method'  => 'PUT',
+			'headers' => $headers,
+			'body'    => wp_json_encode( array( 'refreshToken' => $this->refresh_token ) ),
 		);
-		$response = wp_remote_request($this->refresh_url, $args);
-		$body = $response[body];
-		$body_array = json_decode($body);
+		$response           = wp_remote_request( $this->refresh_url, $args );
+		$body               = $response[ body ];
+		$body_array         = json_decode( $body );
 		$this->access_token = $body_array->AccessToken;
-		putenv("SHIFTER_ACCESS_TOKEN=$this->access_token");
+		putenv( "SHIFTER_ACCESS_TOKEN=$this->access_token" );
 	}
 
 	private function access_token_is_expired() {
-		$now = new DateTime;
-		$elapsed = self::$token_update_date->diff($now, true);
-		if ($elapsed->h > 1) {
+		$now     = new DateTime();
+		$elapsed = self::$token_update_date->diff( $now, true );
+		if ( $elapsed->h > 1 ) {
 			self::$token_update_date = $now;
 			return true;
 		}
