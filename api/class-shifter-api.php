7de3665aeb141728b926d797df3d89e4c817fa28
Daniel Olson <emaildano@gmail.com>
2019-05-07 11:54:30 -0400

    removed unused constructs

--- api/class-shifter-api.php
+++ api/class-shifter-api.php
@@ -32,34 +32,11 @@ class Shifter_API {
 	// stop.
 
 	/**
-	 * The ID of this plugin.
-	 *
-	 * @since  1.0.0
-	 * @access private
-	 * @var    string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
-
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since  1.0.0
-	 * @access private
-	 * @var    string    $version    The current version of this plugin.
-	 */
-	private $version;
-
-	/**
 	 * Initialize the class and set its properties.
 	 *
 	 * @since 1.0.0
-	 * @param string $plugin_name The name of the plugin.
-	 * @param string $version     The version of this plugin.
 	 */
-	public function __construct( $plugin_name, $version ) {
-
-		$this->plugin_name = $plugin_name;
-		$this->version     = $version;
+	public function __construct() {
 
 		// start.
 		$this->site_id       = getenv( 'SITE_ID' );
